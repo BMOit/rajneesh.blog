09b46ec21436e797e29a31089c1ad87e445ef8fa
BMO <rtbmo23@gmail.com>
2026-01-08 18:30:22 +0530

    chore: add bun & astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://rajneesh.blog',
 	integrations: [mdx(), sitemap()],
 });
